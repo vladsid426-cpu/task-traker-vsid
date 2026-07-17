de868934fe7b9b6b908be09236dbbdf465a9173d
vladsid426-cpu <vladsid426@gmail.com>
2026-07-17 17:21:33 +0000

    Commit

--- task_manager/auth_system/admin.py
+++
@@ -1,7 +0,0 @@
-from django.contrib import admin
-from .models import Register,Login
-# Register your models here.
-
-admin.site.register(Register)
-admin.site.register(Login)
-
--- task_manager/auth_system/apps.py
+++
@@ -1,5 +0,0 @@
-from django.apps import AppConfig
-
-
-class AuthSystemConfig(AppConfig):
-    name = 'auth_system'
